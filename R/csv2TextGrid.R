c440e0288d11dcefc749efbd15377628c30db037
George Moroz <agricolamz@gmail.com>
2024-06-05 12:57:11 +0300

    first commit

---
+++ R/csv2TextGrid.R
@@ -0,0 +1,56 @@
+#' Convert audio to the anotated .csv file
+#'
+#' Adds glosses to the glosses list and adds small capitals to glosses. Escapes strings that begins and ends with curly brackets.
+#'
+#' @author George Moroz <agricolamz@gmail.com>
+#'
+#' @param name character vector with the name of the .csv file
+#' @param speaker character vector with the speaker name that is used as the tier name.
+#'
+#' @return writes a csv file with the annotation
+#'
+#' @importFrom stringr str_glue
+#' @importFrom tools file_ext
+#' @importFrom phonfieldwork get_sound_duration
+#' @importFrom dplyr pull
+#' @importFrom phonfieldwork create_empty_textgrid
+#' @importFrom dplyr rename
+#' @importFrom dplyr select
+#' @importFrom dplyr mutate
+#' @importFrom phonfieldwork df_to_tier
+#'
+#'
+#' @export
+
+csv2TextGrid <- function(name, speaker = "speaker"){
+
+  stringr::str_glue("{name}.csv") |>
+    readr::read_csv() ->
+    df
+
+  files <- list.files(pattern = name)
+
+  file <- files[which(tools::file_ext(files) %in% c("WAV", "wav"))]
+
+  file |>
+    phonfieldwork::get_sound_duration() |>
+    dplyr::pull(duration) ->
+    duration
+
+  phonfieldwork::create_empty_textgrid(duration,
+                        tier_name = "Interviewer",
+                        path = getwd(),
+                        result_file_name = name)
+  df |>
+    dplyr::rename(content = text,
+                  time_start = from,
+                  time_end = to) |>
+    dplyr::select(time_start, time_end, content) |>
+    dplyr::mutate(content = stringr::str_replace_all(content, '"', "'"),
+                  time_start = ifelse(time_start == 0,
+                                      0,
+                                      as.double(time_start)+0.1),
+                  time_end = as.double(time_end)+0.1) |>
+    phonfieldwork::df_to_tier(textgrid = stringr::str_glue("{name}.TextGrid"),
+                              tier_name = speaker)
+}
---
+++ R/speech2text.R
@@ -0,0 +1,48 @@
+#' Convert audio to the anotated .csv file
+#'
+#' Adds glosses to the glosses list and adds small capitals to glosses. Escapes strings that begins and ends with curly brackets.
+#'
+#' @author George Moroz <agricolamz@gmail.com>
+#'
+#' @param audio character vector with the path to the file
+#' @param output_name character vector with the name of the result csv file.
+#' @param model_path character vector with the path to the downloded model.
+#' @param language character vector with the code for the language. See \code{audio.whisper::whisper_languages()} for the list of languages.
+#'
+#' @return writes a csv file with the annotation
+#'
+#' @importFrom stringr str_glue
+#' @importFrom audio.whisper whisper
+#' @importFrom readr write_csv
+#'
+#'
+#' @export
+
+speech2text <- function(audio,
+                        output_name = "output",
+                        model_path = "/home/agricolamz/work/scripts/r/audio.whisper/ggml-large-v2.bin",
+                        language = "ru"){
+
+  # convert to the format specs ---------------------------------------------
+  tmp <- tempdir()
+  stringr::str_glue("ffmpeg -i '{audio}' -ar 16000 -ac 1 -c:a pcm_s16le '{tmp}/{output_name}.wav'") |>
+    system()
+
+  on.exit(file.remove(stringr::str_glue("{tmp}/{output_name}.wav")))
+
+  # load and run model ------------------------------------------------------
+
+  model <- audio.whisper::whisper(model_path)
+  result <- audio.whisper:::predict.whisper(model,
+                                            str_glue("{tmp}/{output_name}.wav"),
+                                            language = language)
+
+  # write down results ------------------------------------------------------
+  result$data |>
+    as.data.frame() |>
+    readr::write_csv(stringr::str_glue("{output_name}.csv"))
+
+  result$params
+  print(result$timing)
+  gc(verbose = FALSE)
+}
